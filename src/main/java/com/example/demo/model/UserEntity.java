7e617fee7d102fb65a2493d703cd98596cd01f66
Abu <iliyanananan@gmail.com>
2023-10-15 19:20:04 +0300

    add hash encoding for user password

--- src/main/java/com/example/demo/model/UserEntity.java
+++ src/main/java/com/example/demo/model/UserEntity.java
@@ -1,48 +1,22 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.example.demo.model.entity.BaseEntity;
+import jakarta.persistence.*;
 
 @Entity
-public class UserEntity {
-    @Id()
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@Table(name = "users")
+public class UserEntity extends BaseEntity {
+    private String firstName;
+
+    private String lastName;
 
     private String email;
 
     private String password;
 
-    private String firstName;
-
-    private String lastName;
-
     private boolean active;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    public UserEntity(){
     }
 
     public String getFirstName() {
@@ -60,6 +34,25 @@ public class UserEntity {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
 
     public boolean isActive() {
         return active;
--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,14 +1,38 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.model.UserEntity;
 import com.example.demo.model.dto.UserRegistrationDTO;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder){
+
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
     @Override
     public void registerUser(UserRegistrationDTO userRegistrationDTO) {
+        userRepository.save(map(userRegistrationDTO));
+    }
 
+
+    private UserEntity map(UserRegistrationDTO userRegistrationDTO){
+        UserEntity userEntity = new UserEntity();
+        userEntity.setActive(true);
+        userEntity.setFirstName(userRegistrationDTO.firstName());
+        userEntity.setLastName(userRegistrationDTO.lastName());
+        userEntity.setEmail(userRegistrationDTO.email());
+        userEntity.setPassword(passwordEncoder.encode(userRegistrationDTO.password()));
+
+
+        return userEntity;
     }
 }
