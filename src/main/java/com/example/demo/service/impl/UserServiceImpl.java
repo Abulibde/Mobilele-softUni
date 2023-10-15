9d5ab7772df46c8b6f59cf55883da712b40b9b6a
Abu <iliyanananan@gmail.com>
2023-10-15 18:13:09 +0300

    anti pattern!

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -9,6 +9,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void registerUser(UserRegistrationDTO userRegistrationDTO) {
-        System.out.println(userRegistrationDTO );
+        System.out.println(userRegistrationDTO); //<---ANTI PATTERN
     }
 }
