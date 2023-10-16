ce2621876ccc460e3f9dd5771c98bdfe146614d8
Abu <iliyanananan@gmail.com>
2023-10-16 12:47:53 +0300

    handle access to offers

---
+++ src/main/java/com/example/demo/model/entity/OfferEntity.java
@@ -0,0 +1,9 @@
+package com.example.demo.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "offers")
+public class OfferEntity extends BaseEntity {
+}
